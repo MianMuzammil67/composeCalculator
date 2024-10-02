a93d51aecc070563103762def81580413a9dc542
mian muzammil <mianmuzammil30@gmail.com>
2024-10-02 23:54:12 +0500

    initial commit

--- app/src/main/java/com/example/calculatorapplication/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapplication/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
